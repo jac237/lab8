cfa66d4611046beaaf7b0da9675f60319ab42cbd
Jessie Cruz <jac237@ucsd.edu>
2020-02-24 23:23:34 -0800

    implemented POST addRSVP fuctnion

--- routes/rsvp.js
+++ routes/rsvp.js
@@ -1,10 +1,25 @@
 var data = {
-  rsvp: ['ixd@ucsd.edu']
+    rsvp: ['ixd@ucsd.edu']
 };
 
 /*
  * GET tasks page.
  */
-exports.adminView = function(request, response){
-  response.render('rsvp', data);
+exports.adminView = function (request, response) {
+    response.render('rsvp', data);
+};
+
+/*
+ * POST controller to handle POST requests.
+ */
+exports.addRSVP = function (request, response) {
+    var rsvpEmail = request.body.rsvpEmail;
+    // Will print email to terminal when POST request is made
+    console.log(rsvpEmail);
+
+    data.rsvp.push(rsvpEmail); // Add to current data
+
+    // Let us send back some data so that
+    // browser knows it worked!
+    response.send(rsvpEmail);
 };

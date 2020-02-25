6e32002b9a3ec8b1e2a8a6db4da63a7f1b323dec
Jessie Cruz <jac237@ucsd.edu>
2020-02-24 20:58:02 -0800

    implemented initMap function to show api

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,15 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = 'Yc7G0sye6KleB4hliFl4qV0iPxuCY5pa';
+
+	// 'map' refers to a <div> element with the ID map
+	var location = [32.8778504,-117.2368428];
+
+	L.mapquest.map('map', {
+		center: location,
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 14,
+		zoomControl: false
+	});
+
+	L.marker(location).addTo(map);
 }

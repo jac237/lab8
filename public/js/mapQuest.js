5316c9e5e34eb0481875a49e5ebe299fa2635f4f
Jessie Cruz <jac237@ucsd.edu>
2020-02-24 21:50:00 -0800

    set up map location and marker

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -2,14 +2,14 @@ function initMap() {
 	L.mapquest.key = 'Yc7G0sye6KleB4hliFl4qV0iPxuCY5pa';
 
 	// 'map' refers to a <div> element with the ID map
-	var location = [32.8778504,-117.2368428];
+	// var location = [32.8778504,-117.2368428];
 
-	L.mapquest.map('map', {
-		center: location,
+	var map = L.mapquest.map('map', {
+		center: [32.8778504,-117.2368428],
 		layers: L.mapquest.tileLayer('map'),
 		zoom: 14,
 		zoomControl: false
 	});
 
-	L.marker(location).addTo(map);
+	L.marker([32.8778504,-117.2368428]).addTo(map);
 }
